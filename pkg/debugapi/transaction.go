6155ed92a336c1bb05deb6b71b58697ccb694625
Ralph Pichler <pichler.ralph@gmail.com>
2021-03-22 22:20:58 +0100

    txboost

--- pkg/debugapi/transaction.go
+++ pkg/debugapi/transaction.go
@@ -116,3 +116,7 @@ func (s *Service) transactionResendHandler(w http.ResponseWriter, r *http.Reques
 
 	jsonhttp.OK(w, txHash)
 }
+
+func (s *Service) transactionBoostHandler(w http.ResponseWriter, r *http.Request) {
+
+}
